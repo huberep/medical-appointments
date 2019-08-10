3862ab6c39859dd15853cdb0800dd6e26587f89a
Huber Espinoza <huberep@gmail.com>
2019-08-10 07:06:51 -0600

    Creating Medical Appointments Solution

---
+++ MedicalAppointments/MedicalAppointments/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalAppointments.Startup))]
+namespace MedicalAppointments
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
